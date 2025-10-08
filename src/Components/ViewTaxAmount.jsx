46f808e74992a7012ffc16c68849ecdc7a5ab859
Sadman Wasee <sadman.wasee125@gmail.com>
2025-10-08 12:14:31 +0600

    Made the calculateTax algorithm more efficient

--- src/Components/ViewTaxAmount.jsx
+++ src/Components/ViewTaxAmount.jsx
@@ -1,22 +1,14 @@
 import React, { useContext } from 'react'
 import { FormContext } from '../Context/AppContext'
-import { categories } from '../Utils/AppUtils';
+import { categories, taxBrackets, taxExemptionLimit} from '../Utils/AppUtils';
+
 
 function ViewTaxAmount() {
 
   const { form, setFormData } = useContext(FormContext);
-  let taxExemptionCategory6 = 375000;
   let annualIncome = 0;
   let total = 0;
   let taxPayable = 0;
-  let taxAmount = {
-    "bracket1": 0,
-    "bracket2": 0,
-    "bracket3": 0,
-    "bracket4": 0,
-    "bracket5": 0,
-    "bracket6": 0
-  }
 
   if (form.incomeType == "0") {
     let income = form.income * 12
@@ -33,66 +25,54 @@ function ViewTaxAmount() {
 
 
   if (form.category == "parentOfADisabledChild") {
-    taxExemptionCategory6 = taxExemptionCategory6 + (50000 * form.numberOfDisabledChildren);
+    taxExemptionLimit["parentOfADisabledChild"] = taxExemptionLimit["parentOfADisabledChild"] + (50000 * form.numberOfDisabledChildren);
   }
 
-  const taxExemptionLimit = {
-    "generalTaxPayers": 375000,
-    "women&seniorCitizens": 425000,
-    "peopleWithDisabilities": 500000,
-    "thirdGenderIndividuals": 500000,
-    "warWoundedFreedomFighters": 525000,
-    "parentOfADisabledChild": taxExemptionCategory6
-  }
 
-  const calculateTax = (annualIncome, taxExemption, taxAmount) => {
 
-    if (annualIncome > taxExemption) {
-      let taxedAmount = taxExemption
+  const calculateTax = (annualIncome, taxExemption, taxBrackets) => {
 
-      if (annualIncome < (taxedAmount + 300000)) {
-        taxAmount['bracket2'] = (annualIncome - taxedAmount) * 0.1
-      } else {
-        taxedAmount += 300000
+    let incomeLeft = annualIncome;
 
-        if (annualIncome < (taxedAmount + 400000)) {
-          taxAmount['bracket3'] = (annualIncome - taxedAmount) * 0.15
-          taxAmount['bracket2'] = 300000 * 0.10
+    if (incomeLeft > taxExemption) {
+
+      incomeLeft = incomeLeft - taxExemption
+      let loopCount = taxBrackets.length
+
+      for (let i = 0; i < loopCount; i++) {
+
+        if (i == 0) {
+          continue;
+        }
+
+        if (i === taxBrackets.length - 1) {
+          taxBrackets[i].tax = incomeLeft * taxBrackets[i].rate
+          incomeLeft = 0;
         } else {
-          taxedAmount += 400000
-
-          if (annualIncome < (taxedAmount + 500000)) {
-            taxAmount['bracket4'] = (annualIncome - taxedAmount) * 0.20
-            taxAmount['bracket3'] = 400000 * 0.15
-            taxAmount['bracket2'] = 300000 * 0.10
+          if (incomeLeft > taxBrackets[i].limit) {
+            taxBrackets[i].tax = taxBrackets[i].limit * taxBrackets[i].rate
+            incomeLeft = incomeLeft - taxBrackets[i].limit
           } else {
-            taxedAmount += 500000
-
-            if (annualIncome < (taxedAmount + 2000000)) {
-              taxAmount['bracket5'] = (annualIncome - taxedAmount) * 0.25
-              taxAmount['bracket4'] = 500000 * 0.20
-              taxAmount['bracket3'] = 400000 * 0.15
-              taxAmount['bracket2'] = 300000 * 0.10
-            } else {
-              taxedAmount += 2000000
-
-              taxAmount['bracket6'] = (annualIncome - taxedAmount) * 0.30
-              taxAmount['bracket5'] = 2000000 * 0.25
-              taxAmount['bracket4'] = 500000 * 0.20
-              taxAmount['bracket3'] = 400000 * 0.15
-              taxAmount['bracket2'] = 300000 * 0.10
-            }
+            taxBrackets[i].tax = incomeLeft * taxBrackets[i].rate
+            incomeLeft = 0
           }
         }
-      }
-    }
 
-    total = taxAmount['bracket6'] + taxAmount['bracket5'] + taxAmount['bracket4'] + taxAmount['bracket3'] + taxAmount['bracket2'] + taxAmount['bracket1']
+        if (incomeLeft <= 0) {
+          break;
+        }
+
+      }
+
+      for (let i = 0; i < loopCount; i++) {
+        total += taxBrackets[i].tax
+      }
+
+    }
 
   }
 
-  calculateTax(annualIncome, taxExemptionLimit[form.category], taxAmount)
-
+  calculateTax(annualIncome, taxExemptionLimit[form.category], taxBrackets)
 
   return (
     <>
@@ -116,32 +96,32 @@ function ViewTaxAmount() {
               <tr>
                 <td>Up to {taxExemptionLimit[form.category]}</td>
                 <td>0%</td>
-                <td>{taxAmount["bracket1"]}</td>
+                <td>{taxBrackets[0].tax}</td>
               </tr>
               <tr>
                 <td>Next 300,000</td>
                 <td>10%</td>
-                <td>{taxAmount["bracket2"]}</td>
+                <td>{taxBrackets[1].tax}</td>
               </tr>
               <tr>
                 <td>Next 400,000</td>
                 <td>15%</td>
-                <td>{taxAmount["bracket3"]}</td>
+                <td>{taxBrackets[2].tax}</td>
               </tr>
               <tr>
                 <td>Next 500,000</td>
                 <td>20%</td>
-                <td>{taxAmount["bracket4"]}</td>
+                <td>{taxBrackets[3].tax}</td>
               </tr>
               <tr>
                 <td>Next 2,000,000</td>
                 <td>25%</td>
-                <td>{taxAmount["bracket5"]}</td>
+                <td>{taxBrackets[4].tax}</td>
               </tr>
               <tr>
                 <td>Above 3,575,000</td>
                 <td>30%</td>
-                <td>{taxAmount["bracket6"]}</td>
+                <td>{taxBrackets[5].tax}</td>
               </tr>
               <tr>
                 <td colSpan='2'>Minimum tax payable in BDT:</td>
--- src/Utils/AppUtils.js
+++ src/Utils/AppUtils.js
@@ -6,3 +6,21 @@ export const categories = {
     "warWoundedFreedomFighters": "War-wounded Freedom Fighters",
     "parentOfADisabledChild": "Parent of a Disabled Child"
 }
+
+export let taxBrackets = [
+    { id: 1, limit: 375000, rate: 0, tax: 0 },
+    { id: 2, limit: 300000, rate: 0.1, tax: 0 },
+    { id: 3, limit: 400000, rate: 0.15, tax: 0 },
+    { id: 4, limit: 500000, rate: 0.20, tax: 0 },
+    { id: 5, limit: 2000000, rate: 0.25, tax: 0 },
+    { id: 6, limit: 3575000, rate: 0.30, tax: 0 }
+]
+
+let taxExemptionLimit = {
+    "generalTaxPayers": 375000,
+    "women&seniorCitizens": 425000,
+    "peopleWithDisabilities": 500000,
+    "thirdGenderIndividuals": 500000,
+    "warWoundedFreedomFighters": 525000,
+    "parentOfADisabledChild": 375000
+}
